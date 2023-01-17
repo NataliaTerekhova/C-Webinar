bbbc1c5d95966b25fce11a40aab3ea48e093e6af
Natalia <natalia_terekhova@yahoo.com>
2023-01-17 13:08:59 +0300

    Add Task 4

--- Task1/Task3/Program.cs
+++ Task1/Task3/Program.cs
@@ -1,9 +1,12 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа на промежутке от -N до N
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int currentNumber=number*-1;
-while (currentNumber<=number)
+
+for (int currentNumber = number * -1; currentNumber <= number; currentNumber++)
 {
-    System.Console.Write(currentNumber + " "); 
-    currentNumber++;
+    Console.Write (currentNumber + " ");
 }
+
+
+
+
---
+++ Task1/Task4/Program.cs
@@ -0,0 +1,5 @@
+﻿// напишите программу, которая на входе получает трехзначное число, а на входе выдает его последнюю цифру
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = number%10;
+System.Console.WriteLine(lastDigit);
